646a79b798f7f9b1f803d5d85311d6e8438487bf
vovazavislyak <vowa222216@gmail.com>
2021-01-16 12:45:50 +0200

    Created UserRepository

---
+++ ToDoList/Services/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Services
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ToDoContext _context;
+
+        public UserRepository(ToDoContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<bool> ContainsUserWithEmailAsync(string email)
+        {
+            return await _context.Users.AnyAsync(user => user.Email == email);
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> IsCorrectEmailAndPasswordAsync(string email, string password)
+        {
+            return await _context.Users.AnyAsync(user => user.Email == email && user.Password == password);
+        }
+
+        public async Task<User> GetUserAsync(string email)
+        {
+            return await _context.Users.FirstAsync(user => user.Email == email);
+        }
+    }
+}
