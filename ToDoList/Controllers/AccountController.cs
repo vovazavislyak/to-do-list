fd8c9b257297361c130aea65ab89c169a8321976
vovazavislyak <vowa222216@gmail.com>
2021-01-16 12:34:07 +0200

    Created AccountController

---
+++ ToDoList/Controllers/AccountController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using ToDoList.Services;
+using ToDoList.ViewModels;
+
+namespace ToDoList.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AccountController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            
+            if (_userRepository.ContainsUserWithEmail(model.Email))
+            {
+                ModelState.AddModelError("", "Incorrect login and (or) password");
+                return View(model);
+            }
+
+            var user = new User {Email = model.Email, Password = model.Password};
+
+            await _userRepository.AddUserAsync(user);
+
+            await Authenticate(user);
+
+            return RedirectToAction(nameof(ToDoController.GetAllTask), "ToDo");
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (_userRepository.IsCorrectEmailAndPassword(model.Email, model.Password))
+            {
+                var user = _userRepository.GetUser(model.Email);
+                
+                await Authenticate(user);
+                
+                return RedirectToAction(nameof(ToDoController.GetAllTask), "ToDo");
+            }
+            ModelState.AddModelError("", "Incorrect login and(or) password");
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+
+        private async Task Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(nameof(Models.User.Id), user.Id.ToString()),
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email)
+            };
+
+            var identity = new ClaimsIdentity(claims, "ApplicationCookie",
+                ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(identity));
+        }
+    }
+}
