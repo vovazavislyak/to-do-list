7c7a39b0e9c8f50b1b15b0815583a8e284741312
vovazavislyak <vowa222216@gmail.com>
2021-01-16 10:55:26 +0200

    Created ToDoContext

---
+++ ToDoList/Data/ToDoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Data
+{
+    public sealed class ToDoContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+        
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
