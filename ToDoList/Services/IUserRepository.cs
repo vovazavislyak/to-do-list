3f150c3521205a6e2be44fae40c895175e348e46
vovazavislyak <vowa222216@gmail.com>
2021-01-16 12:34:33 +0200

    Created IUserRepository

---
+++ ToDoList/Services/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using ToDoList.Models;
+
+namespace ToDoList.Services
+{
+    public interface IUserRepository
+    {
+        bool ContainsUserWithEmail(string email);
+
+        Task AddUserAsync(User user);
+
+        bool IsCorrectEmailAndPassword(string email, string password);
+        
+        User GetUser(string email);
+    }
+}
