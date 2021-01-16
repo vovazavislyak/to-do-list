3ddbb28ac237677a6e848376a899071e09a27cc0
vovazavislyak <vowa222216@gmail.com>
2021-01-16 12:19:17 +0200

    Created LoginViewModel and RegisterViewModel

---
+++ ToDoList/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+        [Required]
+        [Compare(nameof(Password), ErrorMessage = "Passwords don't match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
